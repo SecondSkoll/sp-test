cc02cde06ea72ff75eec32634c22e4ed97b33d92
secondskoll <michael.park@canonical.com>
2024-12-04 16:40:26 +1000

    remove timestamp extension for testing

--- docs/conf.py
+++ docs/conf.py
@@ -209,7 +209,6 @@ linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
 
 # myst_enable_extensions = set()
 
-
 # Custom Sphinx extensions; see
 # https://www.sphinx-doc.org/en/master/usage/extensions/index.html
 
@@ -230,8 +229,7 @@ linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
 
 extensions = [
     "canonical_sphinx",
-    "sphinxcontrib.cairosvgconverter",
-    "sphinx_last_updated_by_git"
+    "sphinxcontrib.cairosvgconverter"
 ]
 
 
